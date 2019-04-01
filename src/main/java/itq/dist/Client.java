32d944816b7cc45e20799e7faf4b210b824a8222
Angel Leon <luianglenlop@gmail.com>
2019-03-31 18:01:21 -0600

    moved files

---
+++ src/main/java/itq/dist/Client.java
@@ -0,0 +1,25 @@
+package itq.dist;
+
+public class Client
+{
+
+    public static void main(String[] args)
+    {
+
+        // client type 1 (without sigup) but... with all steps
+        Thread t_client = new ThreadClient(1, 0);
+        t_client.start();
+        // client type 2 ...with sigup!, with all steps
+        Thread t_client2 = new ThreadClient(2, 0);
+        t_client2.start();
+
+        int step = 0;
+        Thread tClientStep = new ThreadClient(1, step);
+        tClientStep.start();
+
+        Thread tClientStep2 = new ThreadClient(2, step);
+        tClientStep2.start();
+
+    }
+
+}
